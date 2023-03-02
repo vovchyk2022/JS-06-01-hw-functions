788a9a4fbeaede2ac15d6b54062da18f1e891d50
Volodymyr Kapushchak <poshta070887@gmail.com>
2023-03-02 20:59:48 +0200

    Add last changes

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-// //  написати функцію, яка повертає слово 'odd' або 'even', в залежності від прийнятого аргументу(число).
+//  написати функцію, яка повертає слово 'odd' або 'even', в залежності від прийнятого аргументу(число).
 
-// /**
-//  * 
-//  * @param {any} value 
-//  * @returns {boolean}
-//  */
-// function checkNumber(value){
-//   const checkValue = value ==='' || value === null || Number.isNaN(Number(value))
-//   return !checkValue;
-// }
-
-
-// const number = prompt("enter number:", 1);
-// if (checkNumber(number)){
-//   console.log(checkParity(Number(number)));
-// } else {
-//   console.log("Error, value must be number");
-// }
+/**
+ * 
+ * @param {any} value 
+ * @returns {boolean}
+ */
+function checkNumber(value){
+  const checkValue = value ==='' || value === null || Number.isNaN(Number(value))
+  return !checkValue;
+}
 
 
-// /**
-//  *
-//  * @param {number} number
-//  * @returns {string}
-//  */
-// //  function retParityWord(number = prompt()) {
-//   function checkParity(number) {
-//   if (number % 2 === 0) {
-//     return "even";
-//   }
-//   return "odd";
-// };
+const number = prompt("enter number:", 1);
+if (checkNumber(number)){
+  console.log(checkParity(Number(number)));
+} else {
+  console.log("Error, value must be number");
+}
+
+
+/**
+ *
+ * @param {number} number
+ * @returns {string}
+ */
+//  function retParityWord(number = prompt()) {
+  function checkParity(number) {
+  if (number % 2 === 0) {
+    return "even";
+  }
+  return "odd";
+};
 
 
 
@@ -59,24 +59,4 @@ function compareTwoValue(value1, value2) {
 }
 
 
-// // Варіант2 - неправильно
-
-// const value1 = '1414';
-// const value2 = 14;
-// console.log(compareTwoValue(value1,value2));
-// /**
-//  *
-//  * @param {any} value1
-//  * @param {any} value2
-//  * @returns {any | null}
-//  */
-// // debugger
-// function compareTwoValue(value1, value2) {
-//   if (typeof value1 !== typeof value2 || value1 === value2) {
-//     if (value1 > value2)
-//     return value1;
-//     return value2;
-// }
-// return null 
-//   }
 
